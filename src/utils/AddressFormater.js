90d19765d578275745ecacc366b699e103283d25
David Lefrancq <david.lefrancq@gmail.com>
2021-06-30 23:50:03 +0200

    [refactor] Delete concolo.log in AddressFormater

--- src/utils/AddressFormater.js
+++ src/utils/AddressFormater.js
@@ -1,14 +1,10 @@
 class AddressFormater {
     static minimizer(address){
-        console.log(address);
         let miniAddress = "";
         if (address && address.length > 10) {
             const start = address.slice(0, 5);
-            console.log(start);
             const end = address.slice((address.length - 5), (address.length));
-            console.log(end);
             miniAddress = start + "..." + end;
-            console.log(miniAddress);
         }
         return miniAddress;
     }

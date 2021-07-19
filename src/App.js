b0762a3c1f5050bacc1f033cf8ee4f8834af10d5
David Lefrancq <david.lefrancq@gmail.com>
2021-07-19 14:25:03 +0200

    [feat] Update Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Vote from "./components/Vote";
+import {GiJigsawBox} from "react-icons/all";
 
 function App() {
     return (
         <div className="App">
 
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
+                <GiJigsawBox size={64}/>
+                <h1>
                     Crypto Vote
-                </p>
+                </h1>
+                <h2 className={"fst-italic"}>{'{ '}Prototype{' }'}</h2>
             </header>
 
             <Vote/>
